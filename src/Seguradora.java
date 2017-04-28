68ab84bb375efc67aaeea7f3d7a2ad0f48a49b82
Aluizio Neto <aluiziooo@users.noreply.github.com>
2017-04-28 17:32:43 -0300

    Commit final

---
+++ src/Seguradora.java
@@ -0,0 +1,37 @@
+
+public class Seguradora {
+/*
+ * Provaveis funcionalidades	
+	void cadastroClientes(){
+		
+	}
+	
+	void cadastroContratos(){
+		
+	}
+	
+	void listaClientes(){
+		
+	}
+	
+	void listaContratos(){
+		
+	}
+	
+	void cadastroSinistro(){
+		
+	}
+	
+	void listaSinistro(){
+		
+	}
+	
+	void listaContratosSemSinistro(){
+		
+	}
+
+	void persistenciaSerialização(){
+		
+	}
+*/
+}
